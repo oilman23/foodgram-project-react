ead1cc4adfcfacae688a6f9aa4d33abe11722da4
unknown <Vlad-swimer1995@yandex.ru>
2022-10-23 12:19:44 +0300

    v1.0

--- backend/api/management/commands/loadjson.py
+++ backend/api/management/commands/loadjson.py
@@ -2,7 +2,7 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from api.models import Ingredient
+from api.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
@@ -11,9 +11,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_path = options["path"]
+        print(file_path)
         with open(file_path, encoding="utf-8") as f:
             data = json.load(f)
-            for item in data:
-                Ingredient.objects.create(
-                    name=item["name"],
-                    measurement_unit=item["measurement_unit"])
+            if file_path == "ingredients.json":
+                for item in data:
+                    Ingredient.objects.create(
+                        name=item["name"],
+                        measurement_unit=item["measurement_unit"])
+            else:
+                for item in data:
+                    Tag.objects.create(
+                        name=item["name"],
+                        color=item["color"],
+                        slug=item["slug"],)
